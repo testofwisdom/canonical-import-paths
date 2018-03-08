5c5082232c86d594eaff45956c5f10386fcded33
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-03-08 13:19:32 -0700

    root

--- go/src/example.com/canonical.go
+++ go/src/example.com/canonical.go
@@ -1,6 +1,6 @@
 // Copyright (c) Me Company Ltd, All Rights Reserved
 
-package lib // import "go/src/example.com/lib"
+package lib // import "go/src/example.com"
 
 // This magical comment lets Sourcegraph know that this package is located at
 // "example.com/lib", and as such an import like "example.com/foobar" can be
